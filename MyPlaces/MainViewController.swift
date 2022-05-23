c21d3e9c6cc84d18094337eb66146cfb6b25a98a
TearsOprah <qmeshokp@mail.ru>
2022-05-23 19:56:29 +0300

    Add table view

---
+++ MyPlaces/MainViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by meshok on 23.05.2022.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    let restaurantNames = ["Buzzols", "Shau", "Шаулинь", "Данар", "Бомжатка"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restaurantNames.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        
+
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
